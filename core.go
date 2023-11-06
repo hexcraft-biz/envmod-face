6892103cfaa7ff2a683b93999107face0d6016c4
Boss <cure1218@gmail.com>
2023-11-06 16:51:57 +0000

    boss

--- core.go
+++ core.go
@@ -1,9 +1,11 @@
 package face
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/Kagami/go-face"
+	"github.com/hexcraft-biz/her"
 )
 
 // ================================================================
@@ -23,6 +25,17 @@ func New() (*Face, error) {
 // ================================================================
 //
 // ================================================================
+func (e Face) RecognizeSingle(jpegbytes []byte) (*face.Face, *her.Error) {
+	faces, err := e.Recognizer.Recognize(jpegbytes)
+	if err != nil {
+		return nil, her.NewError(http.StatusInternalServerError, err, nil)
+	} else if len(faces) != 1 {
+		return nil, her.NewErrorWithMessage(http.StatusBadRequest, "Can only have one face in the image", nil)
+	}
+
+	return &faces[0], nil
+}
+
 func (e *Face) Open() error {
 	var err error
 	e.Close()
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/hexcraft-biz/envmod-face
 go 1.21.1
 
 require github.com/Kagami/go-face v0.0.0-20210630145111-0c14797b4d0e
+
+require github.com/hexcraft-biz/her v0.0.3 // indirect
