695df9ac2cc51565b8af5b21e24dfc062c49ec24
Boss <cure1218@gmail.com>
2023-12-08 16:08:02 +0000

    boss

--- core.go
+++ core.go
@@ -1,6 +1,9 @@
 package face
 
 import (
+	"bytes"
+	"database/sql/driver"
+	"encoding/binary"
 	"net/http"
 	"os"
 
@@ -51,5 +54,56 @@ func (e *Face) Open() error {
 func (e *Face) Close() {
 	if e.Recognizer != nil {
 		e.Recognizer.Close()
+	}
+}
+
+// ================================================================
+//
+// ================================================================
+const (
+	DimensionCount    = 128
+	FaceDistThreshold = 0.15
+)
+
+type Descriptor face.Descriptor
+
+func (d Descriptor) Value() (driver.Value, error) {
+	buf := new(bytes.Buffer)
+	if err := binary.Write(buf, binary.LittleEndian, d); err != nil {
+		return nil, err
+	} else {
+		return buf.Bytes(), nil
+	}
+}
+
+func (d *Descriptor) Scan(src any) error {
+	if src != nil {
+		buf := bytes.NewReader(src.([]byte))
+		return binary.Read(buf, binary.LittleEndian, d)
+	}
+
+	return nil
+}
+
+func (d Descriptor) DistWithFace(f *Descriptor) float64 {
+	sum, diff := float64(0), float64(0)
+	for i := 0; i < DimensionCount; i += 1 {
+		diff = float64(d[i] - (*f)[i])
+		sum += diff * diff
+	}
+
+	return sum
+}
+
+// ================================================================
+type Threshold float64
+
+func (t *Threshold) Validate() {
+	if *t == 0.0 {
+		*t = FaceDistThreshold
+	} else if *t < 0.0 {
+		*t = 0.01
+	} else if *t > 0.99 {
+		*t = 0.99
 	}
 }
